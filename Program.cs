3ae59d5d3d38bdcff6170a9d03fb16425fd23cc5
mjer21-pixel <mangaron.jeremiah21@gmail.com>
2026-03-21 03:12:26 +0800

    Initial commit - ASP.NET Core Web API Pizza Store Project with In Memory database

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<PizzaDb>(opts => opts.UseInMemoryDatabase("PizzaDb"));
+builder.Services.AddSwaggerGen( c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Pizza Store API",
+        Description = "Making the Pizzas you love",
+        Version = "v1"
+    });
+});
+
+var app = builder.Build();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(opts =>
+    {
+        opts.SwaggerEndpoint("/swagger/v1/swagger.json", "Pizza Store API");
+    });
+}
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
